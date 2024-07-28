95d195bdd33d492d8be724e72188ef004c4e10ab
saidAbdirahman <saidqaaje11@gmail.com>
2024-07-28 13:36:33 +0200

    Ref templates manipulatied data  using compostion api
    Signed-off-by: saidAbdirahman <saidqaaje11@gmail.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+home
+<p ref="p">My name is {{name}} and my age is {{age}}</p>
+<button @click="handleEvent()">click me </button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  setup(){
+    console.log('setuped')
+    let name = 'said'
+    let age = 21
+
+    const p = ref(null)
+    const handleEvent = () => {
+      console.log(p,p.value)
+      p.value.classList.add('test')
+      p.value.textContent ='Hello, Said'
+    }
+
+    return {
+      name,
+      age,
+      handleEvent:handleEvent,
+      p
+    }
+  } ,
+  
 }
 </script>
